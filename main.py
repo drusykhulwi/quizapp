59be62038b12c9ca75617e73c52421c49d5cc3f8
Drusilla Khulwael <102147801+drusykhulwi@users.noreply.github.com>
2024-11-23 10:42:47 +0300

    the questions

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-questions = ()
+questions = (
+    "What is an algorithm?"
+    "How do you develop and test an algorithm?"
+    "What is inheritance?"
+    "How does inheritance work?"
+    "What is the difference between statically and dynamically typed programming?"
+)
 
 # make a 2D tuple of answers
-options = ((), (), (), (), ()) 
+options = (
+    (), 
+    (), 
+    (), 
+    (), 
+    ()
+) 
 
 answers = ()
 
