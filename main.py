befd632529d656580cff2df68612fe21a9936070
Drusilla Khulwael <102147801+drusykhulwi@users.noreply.github.com>
2024-11-23 11:33:54 +0300

    complete quiz game

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 questions = (
-    "Which one is not a state of matter?: "
-    "At what temperature are celcius and farenheit equal?: "
-    "How many bones are in the human body: "
-    "What is the only metal that is liquid at room temperature?: "
-    "What is the chemical symbol for the element gold?: "
+    "Which one is not a state of matter?: ",
+    "At what temperature are celcius and farenheit equal?: ",
+    "How many bones are in the human body: ",
+    "What is the only metal that is liquid at room temperature?: ",
+    "What is the chemical symbol for the element gold?: ",
 )
 
 # make a 2D tuple of answers we have four options
@@ -33,7 +33,7 @@ options = (
         "D.Copper"
     ),
     (
-        "A.CO2", 
+        "A.Be", 
         "B.Au", 
         "C. Li",
         "D.Al"
@@ -47,3 +47,28 @@ guesses = []
 score = 0
 
 question_num = 0
+
+for question in questions:
+    print("********************")
+    print(question)
+    for option in options[question_num]:
+        print(option)
+
+    guess = input("Enter (A, B, C, D): ").upper()
+    guesses.append(guess)
+    if guess == answers[question_num]:
+        score += 1
+        print("CORRECT!")
+    else:
+        print("INCORRECT!")
+        print(f"{answers[question_num]} is the correct answer")    
+    question_num += 1    
+
+score = score / len(questions) * 100
+
+if score >= 70 :
+    print(f"Your score is {score}%, Excellent!")
+elif score >= 50 :
+    print(f"Your score is {score}%,Good Try!")    
+else:
+    print(f"Your score is {score}%,You'll get it next time!")    
