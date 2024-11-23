57ae93b525fe7363e5f2403a1667ad7b317399f2
Drusilla Khulwael <102147801+drusykhulwi@users.noreply.github.com>
2024-11-23 11:00:14 +0300

    options

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
 questions = (
-    "What is an algorithm?"
-    "How do you develop and test an algorithm?"
-    "What is inheritance?"
-    "How does inheritance work?"
-    "What is the difference between statically and dynamically typed programming?"
+    "Which one is not a state of matter?: "
+    "At what temperature are celcius and farenheit equal?: "
+    "How many bones are in the human body: "
+    "What is the only metal that is liquid at room temperature?: "
+    "What is the chemical symbol for the element gold?: "
 )
 
-# make a 2D tuple of answers
+# make a 2D tuple of answers we have four options
 options = (
-    (), 
-    (), 
-    (), 
-    (), 
-    ()
+    (
+        "A. Solid",
+        "B. Gas", 
+        "C. Temperature",
+        "D.Liquid"
+    ), 
+    (
+        "A.-40", 
+        "B.35", 
+        "C.40",
+        "D.0"
+    ),
+    (
+        "A.500", 
+        "B.10", 
+        "C.75",
+        "D.206"
+    ),
+    (
+        "A.Mercury", 
+        "B.Bronze", 
+        "C.Lead",
+        "D.Copper"
+    ),
+    (
+        "A.CO2", 
+        "B.Au", 
+        "C. Li",
+        "D.Al"
+    ),
 ) 
 
 answers = ()
